150a4fb98c295a51eb66b293708f99ad8dd4cf76
tabrezfcoos-maker <tabrezfcoos@gmail.com>
2026-03-04 11:10:36 +0530

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,16 @@
-#require_relative 'languages/archived'
-#require_relative 'languages/active'
-
-#ActiveRecord::Base.transaction do
- # Language.unscoped.delete_all
- # @languages.each_with_index do |language, index|
- #   Language.create(
- #     id: language[:id],
- #     name: language[:name],
- #     is_archived: language[:is_archived],
- #     source_file: language[:source_file],
- #     compile_cmd: language[:compile_cmd],
- #     run_cmd: language[:run_cmd],
- #   )
- # end
-#end
-
-
-languages = []
-
 require_relative 'languages/archived'
 require_relative 'languages/active'
 
-languages = @languages
-
 ActiveRecord::Base.transaction do
-  languages.each do |language|
-    Language.find_or_initialize_by(id: language[:id]).tap do |l|
-      l.name = language[:name]
-      l.is_archived = language[:is_archived]
-      l.source_file = language[:source_file]
-      l.compile_cmd = language[:compile_cmd]
-      l.run_cmd = language[:run_cmd]
-      l.save!
-    end
+  Language.unscoped.delete_all
+  @languages.each_with_index do |language, index|
+    Language.create(
+      id: language[:id],
+      name: language[:name],
+      is_archived: language[:is_archived],
+      source_file: language[:source_file],
+      compile_cmd: language[:compile_cmd],
+      run_cmd: language[:run_cmd],
+    )
   end
 end
